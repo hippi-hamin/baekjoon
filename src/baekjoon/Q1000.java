391035f15f3aacb38e965414b86159f451ad2fe4
hippi-hamin <9908haha@naver.com>
2023-12-04 11:06:00 +0900

    Q1000

---
+++ src/baekjoon/Q1000.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q1000 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = 0;
+		int B = 0;
+		System.out.print("A를 입력하시오 : ");
+		A = sc.nextInt();
+		System.out.print("B를 입력하시오 : ");
+		B = sc.nextInt();
+		System.out.println(A+B);
+		
+	}
+
+}
