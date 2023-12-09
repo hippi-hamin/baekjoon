5c1841423271952fc08f5477414e90827717af33
hippi-hamin <9908haha@naver.com>
2023-12-09 23:39:58 +0900

    Q10926

---
+++ src/baekjoon/Q10430.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q10430 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt(), B = sc.nextInt(), C = sc.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C)+(B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B%C))%C);
+    }
+}
