b9181256e37923f1743f18009eb7f30edd1100f8
hippi-hamin <9908haha@naver.com>
2023-12-31 00:57:07 +0900

    Q25314

---
+++ src/stage3/Q25314.java
@@ -0,0 +1,15 @@
+package stage3;
+
+import java.util.Scanner;
+
+public class Q25314 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int count = N / 4;
+        for (int i = 0; i < count; i++) {
+            System.out.print("long" + " ");
+        }
+        System.out.println("int");
+    }
+}
