1857a383df272e65893ea2e45d48362e45d4d1d3
hippi-hamin <9908haha@naver.com>
2024-01-06 23:34:40 +0900

    Q15552

---
+++ src/stage3/Q15552.java
@@ -0,0 +1,25 @@
+package stage3;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine()," ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            bw.write((A + B) + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
