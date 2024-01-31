4437daaa5a543a305f501a3157ebbdcffc2d497c
hippi-hamin <9908haha@naver.com>
2024-01-31 23:39:26 +0900

    Q25083

---
+++ src/bronze5/Q2338.java
@@ -0,0 +1,15 @@
+package bronze5;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+public class Q2338 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger A = sc.nextBigInteger();
+        BigInteger B = sc.nextBigInteger();
+
+        System.out.println(A.add(B));
+        System.out.println(A.subtract(B));
+        System.out.println(A.multiply(B));
+    }
+}
